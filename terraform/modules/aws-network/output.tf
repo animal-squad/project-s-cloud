55fab45c946b0de853bf227d1b4b0d9b7ae8ebe9
timepresent95 <timepresent95@gmail.com>
2024-10-31 23:21:11 +0900

    Docs: network module 내 subnet 생성을 위한 위존성 및 output 위험 사항 명시

--- terraform/modules/aws-network/output.tf
+++ terraform/modules/aws-network/output.tf
@@ -2,6 +2,7 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
+//FIXME: 순서를 제대로 보장 할 수 있도록 for_each 구문등으로 변경하는것이 안전함.
 output "public_subnet_ids" {
   value = [for idx, az in var.public_subnet_azs : aws_subnet.public[idx].id]
 }
